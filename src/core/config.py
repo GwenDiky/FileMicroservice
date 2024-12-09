48bb3480d0ab68b82a29475a515e911ad77d0235
Tamagil <tamagiltamagil@gmail.com>
2024-12-09 15:34:57 +0300

    feat: check minio health

--- src/core/config.py
+++ src/core/config.py
@@ -1,4 +1,5 @@
 import logging
+from functools import lru_cache
 import sys
 
 from pydantic_settings import BaseSettings
@@ -13,6 +14,7 @@ class MinioSettings(BaseSettings):
     minio_uri: str
 
 
+@lru_cache
 class Settings(BaseSettings):
     minio: MinioSettings = MinioSettings()
 
---
+++ src/core/health.py
@@ -0,0 +1,17 @@
+from fastapi import status
+from requests import get
+from requests.exceptions import ConnectionError
+from src.core.config import settings
+
+
+async def minio_health() -> bool:
+    try:
+        if (
+            get(url=f"http://"
+                    f"{settings.minio.minio_uri}/minio/health/live").status_code
+            == status.HTTP_200_OK
+        ):
+            return True
+        return False
+    except ConnectionError:
+        return False
